b99b2dee1b16aff070169970eb4008fbdcf5cb24
NairritaMohan <nairrita.whitehatjr@gmail.com>
2020-11-29 15:25:21 +0530

    screens added

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
